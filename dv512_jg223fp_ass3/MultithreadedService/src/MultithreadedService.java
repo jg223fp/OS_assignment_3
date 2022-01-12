d74d01d14435e8c34efbba35a6fd7bdf5ee76380
jg223fp <jg223fp@student.lnu.se>
2022-01-12 13:57:55 +0100

    removed unused import

--- dv512_jg223fp_ass3/MultithreadedService/src/MultithreadedService.java
+++ dv512_jg223fp_ass3/MultithreadedService/src/MultithreadedService.java
@@ -3,7 +3,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadLocalRandom;
 
 /*
  * File:	MultithreadedService.java
